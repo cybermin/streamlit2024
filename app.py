067a62945057bfa077bd97b8e40f93873ea31cba
cybermin <sillamin@naver.com>
2024-07-23 16:30:35 +0900

    업다운

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ st.header('업다운게임', divider='rainbow')
 clicked = st.button("게임시작")
 
 n = 0
-if clicked :
+flag = True
+if clicked and flag:
     n = random.randint(1, 100)
+    flag = False
     
 st.write(f'n={n}')
 st.image('./img/what.png')
