4ab316a30477562950c60e850438a7218c9396bd
cybermin <sillamin@naver.com>
2024-07-23 17:38:22 +0900

    메뉴수정

--- app.py
+++ app.py
@@ -1,40 +1,17 @@
 import streamlit as st 
-import random
-import time
-import pandas as pd
+import dice
+import updown
+import df
 
-# 데이터 프레임을 session state로 처리
-if 'df' not in st.session_state :
-   st.session_state.df = pd.read_excel('./성적계산.xlsx')
+st.sidebar.title('메뉴선택')
+page = st.sidebar.radio("",['주사위던지기', '업다운게임','데이터분석'])
 
-st.header('성적계산', divider='rainbow') 
-col1, col2, col3, col4, col5 = st.columns(5)
-
-with col1:
-   bt1 = st.button('성적계산')
-
-with col2:
-   bt2 = st.button('순위계산')
-
-with col3:
-   bt3 = st.button('학점계산') 
-
-with col4:
-   bt4 = st.button('파일저장') 
-
-with col5:
-   bt5 = st.button('성적현황') 
-
-# 데이터 전처리
-st.session_state.df.fillna(0, inplace=True)
-st.session_state.df['학번'] = st.session_state.df['학번'].astype(str).str.replace(',','')
-
-#버튼처리
-if bt1 :
-   st.session_state.df['합계'] = st.session_state.df['출석'] + \
-                                 st.session_state.df['중간'] + \
-                                 st.session_state.df['기말'] + \
-                                 st.session_state.df['과제']  
-                                 
-st.dataframe(st.session_state.df)
-
+if page == '주사위던지기' :
+    st.title['주사위던지기']
+    dice.show()
+elif page == '업다운게임' :
+    st.title['업다운게임']
+    updown.show()
+elif page == '데이터분석' :
+    st.title['데이터분석']
+    df.show()
--- updown.py
+++ updown.py
@@ -2,37 +2,38 @@ import streamlit as st
 import random
 import time
 
-if 'n' not in st.session_state:
-    st.session_state.n = 0
+def show() :
+    if 'n' not in st.session_state:
+        st.session_state.n = 0
 
-if 'flag' not in st.session_state:
-    st.session_state['flag'] = True
+    if 'flag' not in st.session_state:
+        st.session_state['flag'] = True
 
-st.header('업다운게임', divider='rainbow') 
+    st.header('업다운게임', divider='rainbow') 
 
-def start_game() :
-    st.session_state.flag = False
-    st.session_state.n = random.randint(1,100)
-
-if st.session_state['flag'] :
-    # st.button('게임시작', on_click=start_game)
-    if st.button('게임시작') :
+    def start_game() :
         st.session_state.flag = False
-        st.session_state.n = random.randint(1,100) 
-    st.image('./img/what.png')
-else :
-    st.subheader('게임중...')
-    usern = st.text_input("1에서 100까지 정수를 입력",'', placeholder="정수입력")
-    if usern.isdigit() :
-        usern = int(usern)
-        if usern > st.session_state.n : 
-            st.image('./img/down.png')
-        elif usern < st.session_state.n : 
-            st.image('./img/up.png')
-        else :
-            st.image('./img/good.png')
-            st.session_state.flag = True
-            st.session_state.n = None
-            st.success("정답! 5초 후에 초기화")
-            time.sleep(5)
-            st.experimental_rerun()
+        st.session_state.n = random.randint(1,100)
+
+    if st.session_state['flag'] :
+        # st.button('게임시작', on_click=start_game)
+        if st.button('게임시작') :
+            st.session_state.flag = False
+            st.session_state.n = random.randint(1,100) 
+        st.image('./img/what.png')
+    else :
+        st.subheader('게임중...')
+        usern = st.text_input("1에서 100까지 정수를 입력",'', placeholder="정수입력")
+        if usern.isdigit() :
+            usern = int(usern)
+            if usern > st.session_state.n : 
+                st.image('./img/down.png')
+            elif usern < st.session_state.n : 
+                st.image('./img/up.png')
+            else :
+                st.image('./img/good.png')
+                st.session_state.flag = True
+                st.session_state.n = None
+                st.success("정답! 5초 후에 초기화")
+                time.sleep(5)
+                st.experimental_rerun()
