16db47cef85074dd728ab273d888e0557960829f
cybermin <sillamin@naver.com>
2024-07-23 17:10:20 +0900

    성적산출

--- app.py
+++ app.py
@@ -2,37 +2,20 @@ import streamlit as st
 import random
 import time
 
-if 'n' not in st.session_state:
-    st.session_state.n = 0
+st.header('성적계산', divider='rainbow') 
+col1, col2, col3, col4, col5 = st.columns(5)
 
-if 'flag' not in st.session_state:
-    st.session_state['flag'] = True
+with col1:
+   bt1 = st.button('성적계산')
 
-st.header('업다운게임', divider='rainbow') 
+with col2:
+   bt2 = st.button('순위계산')
 
-def start_game() :
-    st.session_state.flag = False
-    st.session_state.n = random.randint(1,100)
+with col3:
+   bt3 = st.button('학점계산') 
 
-if st.session_state['flag'] :
-    # st.button('게임시작', on_click=start_game)
-    if st.button('게임시작') :
-        st.session_state.flag = False
-        st.session_state.n = random.randint(1,100) 
-    st.image('./img/what.png')
-else :
-    st.subheader('게임중...')
-    usern = st.text_input("1에서 100까지 정수를 입력",'', placeholder="정수입력")
-    if usern.isdigit() :
-        usern = int(usern)
-        if usern > st.session_state.n : 
-            st.image('./img/down.png')
-        elif usern < st.session_state.n : 
-            st.image('./img/up.png')
-        else :
-            st.image('./img/good.png')
-            st.session_state.flag = True
-            st.session_state.n = None
-            st.success("정답! 5초 후에 초기화")
-            time.sleep(5)
-            st.experimental_rerun()
+with col4:
+   bt4 = st.button('파일저장') 
+
+with col5:
+   bt5 = st.button('성적현황') 
